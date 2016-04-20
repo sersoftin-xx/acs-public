5afcde8a6518add149ff7dd17ecabf3bdcc9636f
sersoftin@gmail.com <G-TWcAJn3FKLNb>
2016-04-20 22:09:18 +0300

    Edit active_bid.js

--- webroot/js/active_bids.js
+++ webroot/js/active_bids.js
@@ -10,7 +10,7 @@ $('#edit-active-bid-expiration-date-input-group').datetimepicker({
 
 function showEditActiveBidDialog(active_bid_id) {
     if (isFinite(active_bid_id)) {
-        $.get(location.protocol + '//' + window.location.hostname + '/bids/get_info/' + active_bid_id + '.json').success(function (data, textStatus, jqXHR) {
+        $.get(location.protocol + '//' + window.location.hostname + '/bids/get_info/' + active_bid_id + '.json').success(function (data) {
             var bid = data['bid'];
             $('#edit-active-bid-form').attr('action', $('#edit-active-bid-form-action').val() + '/' + bid['id']);
             var product_selector = $('#edit-active-bid-product-id-input');
@@ -21,6 +21,6 @@ function showEditActiveBidDialog(active_bid_id) {
         });
     }
     else {
-        console.log('active_bid_id_id is not a number!');
+        console.log('active_bid_id is not a number!');
     }
 }
