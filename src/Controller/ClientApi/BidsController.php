d7f625417a534d107e3d60e56ceb68d6a24eb0f0
Sergo <sersoftin@gmail.com>
2016-05-28 21:18:04 +0300

    Добавил свойство isExpired, которое отображает, закончился ли срок действия заявки или нет

--- src/Controller/ClientApi/BidsController.php
+++ src/Controller/ClientApi/BidsController.php
@@ -7,7 +7,6 @@ use Cake\Core\Configure;
 use Cake\I18n\Time;
 use Cake\Network\Exception\BadRequestException;
 use Cake\Network\Exception\NotFoundException;
-//use Cake\Utility\Security;
 
 /**
  * Bids Controller
@@ -20,18 +19,9 @@ class BidsController extends AppController
     {
         $this->request->allowMethod('post');
 
-//        $key = Configure::read('Security.encryptionKey');
-//        $data = json_decode(Security::decrypt(base64_decode($this->request->data('data')), $key));
-
-//        $pc_unique_key = $data->pc_unique_key;
-//        $product_id = $data->product_id;
-//        $random_string = $data->random_string;
-//        $pc_name = $data->pc_name;
-
         $pc_unique_key = $this->request->data('pc_unique_key');
         $pc_name = $this->request->data('pc_name');
         $product_id = $this->request->data('product_id');
-//        $random_string = $this->request->data('random_string');
 
         $pc = $this->Bids->Pcs->findOrCreate(['unique_key' => $pc_unique_key],
             function (Pc $pc) use ($pc_name) {
@@ -50,14 +40,8 @@ class BidsController extends AppController
         });
 
         if (!$bid->has('id')) {
-            throw new BadRequestException('BadRequest. Invalid data accepted');
+            throw new BadRequestException('Bid not saved. Invalid data accepted.');
         }
-//        $response = new \stdClass();
-//        $response->bid = $bid;
-//        $response->random_string = $random_string;
-//        $data = Security::encrypt(json_encode($response), $key);
-//        $data = base64_encode($data);
-//        $this->set(compact('data'));
 
         $this->set(compact('bid'));
     }
@@ -66,16 +50,8 @@ class BidsController extends AppController
     {
         $this->request->allowMethod('post');
 
-//        $key = Configure::read('Security.encryptionKey');
-//        $data = json_decode(Security::decrypt(base64_decode($this->request->data('data')), $key));
-
-//        $pc_unique_key = $data->pc_unique_key;
-//        $product_id = $data->product_id;
-//        $random_string = $data->random_string;
-
         $pc_unique_key = $this->request->data('pc_unique_key');
         $product_id = $this->request->data('product_id');
-//        $random_string = $this->request->data('random_string');
 
         $bid = $this->Bids->find('all', [
             'conditions' => [
@@ -89,13 +65,6 @@ class BidsController extends AppController
         } else {
             $bid->unsetProperty('pc');
         }
-
-//        $response = new \stdClass();
-//        $response->bid = $bid;
-//        $response->random_string = $random_string;
-//        $data = Security::encrypt(json_encode($response), $key);
-//        $data = base64_encode($data);
-//        $this->set(compact('data'));
 
         $this->set(compact('bid'));
     }
